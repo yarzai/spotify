04455da7c85274076102caf01563abcaf4a12aae
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2024-08-13 15:23:12 +0430

    Feat: Project Configured

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42c83c);
+  static const lightBackground = Color(0xfff2f2f2);
+  static const darkBackground = Color(0xff0d0c0c);
+  static const grey = Color(0xffbebebe);
+  static const darkGrey = Color(0xff343434);
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/configs/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.lightTheme,
+      home: Container(),
+    );
+  }
+}
