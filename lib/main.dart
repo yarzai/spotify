f21e3a5bc400ec8806fba1adb9cbea25ddd8152b
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2024-08-13 16:28:27 +0430

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify/core/configs/theme/app_theme.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: AppTheme.lightTheme,
-      home: Container(),
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
     );
   }
 }
---
+++ lib/presentation/splash/intro/pages/get_started.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
