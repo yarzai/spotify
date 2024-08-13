e4482042a98c054687283b747406f770d7e5bd42
Aziz Rahman Yarzai <azizulrahman.yarzai@gmail.com>
2024-08-13 16:56:51 +0430

    .

--- lib/presentation/splash/intro/pages/get_started.dart
+++ lib/presentation/splash/intro/pages/get_started.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:spotify/core/assets/app_images.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, image: AssetImage(AppImages.introBG))),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.15),
+          )
+        ],
+      ),
+    );
   }
 }
